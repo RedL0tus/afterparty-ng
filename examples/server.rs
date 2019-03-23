7a1f1450ef18dc8a798f572b94b79a884deec39d
Kay Lin <kaymw@aosc.io>
2019-03-23 11:00:46 -0700

    [WIP] Migrate to hyper 0.12
    Signed-off-by: Kay Lin <kaymw@aosc.io>

--- examples/server.rs
+++ examples/server.rs
@@ -3,17 +3,23 @@ extern crate log;
 extern crate env_logger;
 extern crate afterparty_ng;
 extern crate hyper;
+extern crate futures;
 
+use std::env;
+use futures::Future;
 use afterparty_ng::{Delivery, Hub};
 
 use hyper::Server;
 
 pub fn main() {
-    env_logger::init();
+    if let Err(_) = env::var("AFTERPARTY_LOG") {
+        env::set_var("AFTERPARTY_LOG", "info")
+    }
+    env_logger::init_from_env("AFTERPARTY_LOG");
     let addr = format!("0.0.0.0:{}", 4567);
     let mut hub = Hub::new();
-    hub.handle("pull_request", |delivery: &Delivery| {
-        info!("rec delivery {:#?}", delivery);
+    hub.handle("ping", |delivery: &Delivery| {
+        info!("Received delivery {:#?}", delivery);
         /*match delivery.payload {
             Event::PullRequest { ref action, ref sender, .. } => {
                 info!("sender {} action {}", sender.login, action)
@@ -21,9 +27,9 @@ pub fn main() {
             _ => (),
         }*/
     });
-    let srvc = Server::http(&addr[..])
-                   .unwrap()
-                   .handle(hub);
+    let server = Server::bind(&addr[..].parse().unwrap())
+        .serve(|| Ok(hub))
+        .map_err(|e| error!("Server error: {}", e));
     info!("listening on {}", addr);
-    srvc.unwrap();
+    hyper::rt::run(server);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+#![feature(clone_closures)]
+
 //! Afterparty is a github webhook handler library for building custom integrations
 
 #[macro_use]
 extern crate log;
-#[macro_use]
 extern crate hyper;
 extern crate case;
 extern crate hex;
 extern crate ring;
+extern crate futures;
+
 extern crate serde;
 extern crate serde_json;
 
@@ -15,20 +18,13 @@ mod hook;
 
 pub use events::Event;
 pub use hook::{AuthenticateHook, Hook};
-use hyper::server::{Handler, Request, Response};
+
+use futures::{future, Future};
+use futures::stream::Stream;
+use hyper::service::{Service, NewService};
+use hyper::{Body, Error, Response, StatusCode, Request};
+
 use std::collections::HashMap;
-use std::io::Read;
-
-// signature for request
-// see [this document](https://developer.github.com/webhooks/securing/) for more information
-header! {(XHubSignature, "X-Hub-Signature") => [String]}
-
-// name of Github event
-// see [this document](https://developer.github.com/webhooks/#events) for available types
-header! {(XGithubEvent, "X-Github-Event") => [String]}
-
-// unique id for each delivery
-header! {(XGithubDelivery, "X-Github-Delivery") => [String]}
 
 // A delivery encodes all information about web hook request
 #[derive(Debug)]
@@ -51,11 +47,11 @@ impl<'a> Delivery<'a> {
         let patched = events::patch_payload_json(event, payload);
         match serde_json::from_str::<Event>(&patched) {
             Ok(parsed) => Some(Delivery {
-                id: id,
-                event: event,
+                id,
+                event,
                 payload: parsed,
                 unparsed_payload: payload,
-                signature: signature,
+                signature,
             }),
             Err(e) => {
                 // println!("{}", e);
@@ -82,7 +78,7 @@ impl Hub {
     }
 
     /// adds a new web hook which will only be applied
-    /// when a delivery is revcieved with a valid
+    /// when a delivery is received with a valid
     /// request signature based on the provided secret
     pub fn handle_authenticated<H, S>(&mut self, event: &str, secret: S, hook: H)
     where
@@ -104,8 +100,25 @@ impl Hub {
             .push(Box::new(hook));
     }
 
+    pub fn len(&self) -> usize {
+        self.hooks.len()
+    }
+}
+
+pub struct Worker<'a> {
+    hooks: &'a HashMap<String, Vec<Box<Hook>>>
+}
+
+impl<'a> Worker<'a> {
+    fn from(hooks: &HashMap<String, Vec<Box<Hook>>>) -> Worker {
+        Worker {
+            hooks
+        }
+    }
+
     /// get all interested hooks for a given event
-    pub fn hooks(&self, event: &str) -> Option<Vec<&Box<Hook>>> {
+    fn hooks(&self, event: &str) -> Option<Vec<&Box<Hook>>> {
+        debug!("Finding matches for '{}'", event);
         let explicit = self.hooks.get(event);
         let implicit = self.hooks.get("*");
         let combined = match (explicit, implicit) {
@@ -118,42 +131,93 @@ impl Hub {
         };
         combined
     }
+
+    fn response(scode: StatusCode, resbody: &'static str) -> Box<Future<Item = Response<Body>, Error = Error> + Send> {
+        Box::new(
+            future::ok(
+                Response::builder()
+                    .status(scode)
+                    .body(resbody.into())
+                    .unwrap()
+            )
+        )
+    }
 }
 
-impl Handler for Hub {
-    fn handle(&self, mut req: Request, res: Response) {
-        let headers = req.headers.clone();
-        if let (Some(&XGithubEvent(ref event)), Some(&XGithubDelivery(ref delivery))) = (
-            headers.get::<XGithubEvent>(),
-            headers.get::<XGithubDelivery>(),
-        ) {
-            let signature = headers.get::<XHubSignature>();
-            info!("recv '{}' event with signature '{:?}'", event, signature);
-            if let Some(hooks) = self.hooks(event) {
-                let mut payload = String::new();
-                if let Ok(_) = req.read_to_string(&mut payload) {
-                    match Delivery::new(
-                        delivery,
-                        event,
-                        payload.as_ref(),
-                        signature.map(|s| s.as_ref()),
-                    ) {
-                        Some(delivery) => {
-                            // println!("{:?}", delivery);
-                            for hook in hooks {
-                                hook.handle(&delivery);
-                            }
-                        }
-                        _ => error!(
-                            "failed to parse event {:?} for delivery {:?}",
-                            event, delivery
-                        ),
-                    }
-                }
+impl<'a> Service for Worker<'a> {
+    type ReqBody = Body;
+    type ResBody = Body;
+    type Error = Error;
+    type Future = Box<Future<Item = Response<Body>, Error = Error> + Send>;
+
+    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
+        let headers = req.headers().clone();
+        // Name of Github event and unique ID for each delivery.
+        // See [this document](https://developer.github.com/webhooks/#events) for available types
+        let (event_str, delivery_str) = if let (Some(event), Some(delivery)) = (
+            headers.get("X-GitHub-Event"), headers.get("X-Github-Delivery")) {
+            if let (Ok(event_str), Ok(delivery_str)) = (event.to_str(), delivery.to_str()) {
+                (event_str, delivery_str)
+            } else {
+                error!("Invalid headers");
+                return Worker::response(StatusCode::BAD_REQUEST, "Invalid headers")
             }
+        } else {
+            error!("Invalid request");
+            return Worker::response(StatusCode::BAD_REQUEST, "Invalid Response")
+        };
+        // signature for request
+        // see [this document](https://developer.github.com/webhooks/securing/) for more information
+        let signature = if let Some(value) = headers.get("X-Hub-Signature") {
+            value.to_str().ok()
+        } else {
+            None
+        };
+        info!("Received '{}' event with ID {}", &event_str, &delivery_str);
+        if let Some(hooks) = self.hooks(&event_str) {
+            let body = if let Ok(chunk) = req.into_body().concat2().wait() {
+                chunk
+            } else {
+                error!("Failed to retrieve request body");
+                return Worker::response(StatusCode::BAD_REQUEST, "Failed to retrieve request body")
+            };
+            if let Ok(payload) = String::from_utf8(body.to_vec()) {
+                if let Some(delivery) = Delivery::new(&delivery_str,
+                                                      &event_str,
+                                                      &payload.as_str(),
+                                                      signature) {
+                    for hook in hooks {
+                        hook.handle(&delivery);
+                    }
+                    Worker::response(StatusCode::ACCEPTED, "OK")
+                } else {
+                    error!(
+                        "Failed to parse event {:?} for delivery {:?}",
+                        &event_str, &delivery_str
+                    );
+                    Worker::response(StatusCode::BAD_REQUEST, "Failed to parse event")
+                }
+            } else {
+                error!("Failed to parse request body");
+                Worker::response(StatusCode::BAD_REQUEST, "Failed to parse request body")
+            }
+        } else {
+            error!("No proper hook found");
+            Worker::response(StatusCode::INTERNAL_SERVER_ERROR, "Server not configured")
         }
-        let _ = res.send(b"ok");
-        ()
+    }
+}
+
+impl<'a> NewService for Hub {
+    type ReqBody = Body;
+    type ResBody = Body;
+    type Error = Error;
+    type Service = Worker<'a>;
+    type Future = Box<Future<Item = Self::Service, Error = Self::InitError> + Send>;
+    type InitError = Error;
+
+    fn new_service(&self) -> Self::Future {
+        Box::new(future::ok(Worker::from(&self.hooks)))
     }
 }
 
@@ -171,7 +235,7 @@ mod tests {
         hub.handle("*", |_: &Delivery| {});
         assert_eq!(
             Some(2),
-            hub.hooks("push").map(|hooks| hooks.into_iter().count())
+            hub.len()
         )
     }
 }
